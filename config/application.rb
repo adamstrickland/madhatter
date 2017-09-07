59d716da4f6efaf4f4371681604da589d5b91fd5
Adam Strickland <adam.strickland@gmail.com>
2017-09-07 10:24:53 -0500

    working example

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,19 @@ module Madhatter
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.paperclip_defaults = {
+      storage: :s3,
+      s3_credentials: {
+        access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+        secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+        bucket: ENV.fetch('S3_BUCKET_NAME'),
+        s3_region: ENV.fetch('AWS_REGION'),
+      },
+      url: ":s3_domain_url",
+      path: "/:class/:attachment/:id_partition/:style/:filename",
+    }
+
+    config.action_controller.asset_host = "d2d1a1efa424rj.cloudfront.net"
   end
 end
