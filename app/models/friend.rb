562f85696d97a24aa0d7805938ff24e092657fcb
Adam Strickland <adam.strickland@gmail.com>
2017-09-07 09:11:29 -0500

    saves to s3

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,9 +1,5 @@
 class Friend < ApplicationRecord
-  has_attached_file :avatar, styles: {
-    thumb: '100x100>',
-    square: '200x200#',
-    medium: '300x300>'
-  }
+  has_attached_file :avatar, styles: { square: '200x200#' }
 
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -2,15 +2,12 @@ Rails.application.configure do
   config.paperclip_defaults = {
     storage: :s3,
     s3_credentials: {
-      bucket: ENV.fetch('S3_BUCKET_NAME'),
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
       secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
       s3_region: ENV.fetch('AWS_REGION'),
-    }
+    },
+    url: ":s3_domain_url",
+    path: "/:class/:attachment/:id_partition/:style/:filename",
   }
 end
-
-# Paperclip::Attachment.default_options.tap do |o|
-#   o[:url] = ':s3_domain_url'
-#   o[:path] = '/:class/:attachment/:id_partition/:style/:filename'
-# end
